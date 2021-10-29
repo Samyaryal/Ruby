d4d90b10bb4d10dad69d8b185670205fc95b28ed
Samikshya <samikshyaaryal5@gmail.com>
2021-10-29 11:46:13 +0300

    Finish layout of the application

--- hello_app/app/controllers/application_controller.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationController < ActionController::Base
-  def hello
-    render html: "Sample App"
-  end
-end
--- hello_app/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  # resources :microposts
-  # resources :users
-  # root 'application#hello'
-  root 'static_pages/#hello'
-end
