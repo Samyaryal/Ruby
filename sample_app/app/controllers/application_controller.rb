547f7393e96c78a3e3f8b52633719bf545592581
Samikshya <samikshyaaryal5@gmail.com>
2021-11-06 12:10:31 +0200

    working on user-micropost

--- sample_app/app/controllers/application_controller.rb
+++ sample_app/app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+  private
+  #confirrms a logged-in user
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "please log in."
+      redirect_to login_url
+    end
+  end 
 end
--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -19,12 +19,12 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
+    @users = User.paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless FILL_IN
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -71,4 +71,15 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+   #before filters 
+  def correct_user
+    @user = User.find(params[:id]) 
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  #confirms an admin user
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
